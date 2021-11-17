4d7bfa1f1629f875f4af27b095256d59054c7a8e
Oladele Abeeb <sunsyde2020@gmail.com>
2021-11-17 14:04:18 +0100

    add welcome dialog to routing screen

--- lib/screens/routing_screen.dart
+++ lib/screens/routing_screen.dart
@@ -27,11 +27,25 @@ class _RoutingScreenState extends State<RoutingScreen> {
       final isExist = await PylonsWallet.instance.exists();
       if(isExist){
 
-        // final response = await PylonsWallet.instance.getProfile();
-        // username.value = response.data["username"] ?? "";
-        Future.delayed(const Duration(seconds: 5), (){
-          navigatorKey.currentState!.push(MaterialPageRoute(builder: (_) => const HomeScreen()));
-        });
+        final response = await PylonsWallet.instance.getProfile();
+        username.value = response.data["username"] ?? "";
+        showDialog(
+            context: navigatorKey.currentState!.overlay!.context,
+            barrierDismissible: false,
+            builder: (ctx) => WillPopScope(
+              onWillPop: () => Future.value(false),
+              child: AlertDialog(
+                content: Text("Welcome ${response.data["username"]}"),
+                actions: [
+                  TextButton(onPressed: (){
+                    navigatorKey.currentState!.push(MaterialPageRoute(builder: (_) => const HomeScreen()));
+                  }, child: Text("Ok"))
+                ],
+              ),
+            ));
+        // Future.delayed(const Duration(seconds: 5), (){
+        //   navigatorKey.currentState!.push(MaterialPageRoute(builder: (_) => const HomeScreen()));
+        // });
       }else{
         showDialog(
           context: navigatorKey.currentState!.overlay!.context,
