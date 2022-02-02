1c77ab6105b9ba3b8b1c0369e463492b675d6c52
Tiny @ngel <safe.nysocho305@outlook.com>
2022-02-02 02:12:55 -0500

    fix merge conflict

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,7 @@
 const kImage = "https://images.unsplash.com/photo-1635488640163"
     "-e5f6782cda6e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2576&q=80";
 const kCookbookId = "cookbook_id";
+const kUsername = "username";
 const kShareIcon = 'assets/images/share_ic.png';
 const kSaveIcon = 'assets/images/save_ic.png';
 const ipfsDomain = "https://ipfs.io/ipfs";
