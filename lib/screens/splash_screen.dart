44600ab9532b1e9c4326744e83142eef6e9fb545
Jawad <147865ba@gmail.com>
2022-03-23 20:59:16 +0500

    Solved the issue

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -17,26 +17,21 @@ class SplashScreen extends StatelessWidget {
       children: <Widget>[
         Positioned(left: 0, right: 0, top: 0, bottom: 0, child: SvgPicture.asset(kSvgSplash, fit: BoxFit.cover)),
         Positioned(
-          top: 0,
+          top: 0.3.sh,
           bottom: 0,
           left: 0,
           right: 0,
           child: Container(
             alignment: Alignment.center,
-            child: Container(
-              margin: EdgeInsets.only(top: 0.3.sh),
-              width: 200,
-              height: 50,
-              child: PylonsButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const RoutingScreen()),
-                  );
-                },
-                btnText: kGetStarted,
-                showArrow: false,
-              ),
+            child: PylonsButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RoutingScreen()),
+                );
+              },
+              btnText: kGetStarted,
+              showArrow: false,
             ),
           ),
         ),
