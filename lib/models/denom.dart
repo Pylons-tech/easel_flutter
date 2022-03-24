97f40353c2bcd0e7a40c45afc2ae722c859b23eb
Jawad <147865ba@gmail.com>
2022-03-24 17:06:32 +0500

    Changed the currency order based on stefan request.

--- lib/models/denom.dart
+++ lib/models/denom.dart
@@ -9,13 +9,13 @@ class Denom {
   Denom({required this.name, required this.symbol});
 
   static List<Denom> get availableDenoms => [
-        Denom(name: "Pylon", symbol: kPylonSymbol),
-        Denom(name: "USD", symbol: kUsdSymbol),
-        Denom(name: "Atom", symbol: kAtomSymbol),
-        Denom(name: "EEur", symbol: kEuroSymbol),
-        Denom(name: "Agoric", symbol: kAgoricSymbol),
-        Denom(name: "Juno", symbol: kJunoSymbol),
-        Denom(name: "Luna", symbol: kLunaSymbol),
+        Denom(name: kUSDText, symbol: kUsdSymbol),
+        Denom(name: kPylonText, symbol: kPylonSymbol),
+        Denom(name: kAtomText, symbol: kAtomSymbol),
+        Denom(name: kEurText, symbol: kEuroSymbol),
+        Denom(name: kAgoricText, symbol: kAgoricSymbol),
+        Denom(name: kJunoText, symbol: kJunoSymbol),
+        Denom(name: kLunaText, symbol: kLunaSymbol),
       ];
 
   TextInputFormatter getFormatter() {
