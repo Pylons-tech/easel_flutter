caf65a88d359fbc2c77cd43d8377642e3fb0b0c8
ahmadrns <106388520+ahmadrns@users.noreply.github.com>
2022-06-20 21:58:32 -0700

    changes mentioned in PR

--- lib/services/third_party_services/audio_player_helper.dart
+++ lib/services/third_party_services/audio_player_helper.dart
@@ -1,34 +1,34 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
 /// Abstract Class for providing audio player
 abstract class AudioPlayerHelper {
   ///This method works as an intializer for Audio Player,
-  ///It will allow us to pass the url for the Audio
-  ///Returns true if Url is loaded otherwise false
+  ///Input: [String] as path of audio
+  ///Output: [bool] tells weather the audio is set or not
   Future<bool> setFile({required String file});
 
   ///This method works as an initializer for Audio Player,
-  ///It will take file path as input
-  ///It will load the specific file for further operations like [Play], [Pause], [Resume]
+  ///Input: [File] file of selected audio
   Future<void> setAudioSource({required File file});
 
   /// This method will be a listener to the playerStateStream
-  /// which will listen the audio stream being player
+  /// Output: [Stream] of [PlayerState] data returned
   Stream<PlayerState> playerStateStream();
 
-  /// This method will be a listener to the positionStream which
-  /// will listen to the current DurationPosition of the audio track
+  /// This method will be a listener to the positionStream
+  /// Output: [Stream] of [Duration] of Position Stream returned
   Stream<Duration> positionStream();
 
-  /// This method will be a listener to the bufferedPositionStream which
-  /// will listen to the duration position where the audio is being buffered
+  /// This method will be a listener to the bufferedPositionStream
+  /// Output: [Stream] of [Duration] of Buffered Position Stream
   Stream<Duration> bufferedPositionStream();
 
   /// This method will be a listener to the durationStream which
-  /// will listen to the DurationStream being player
+  /// Output: [Stream] of [Duration] of current audio returned
   Stream<Duration?> durationStream();
 
   /// This method will be responsible for destroying the audio player instances from the memory
@@ -41,8 +41,7 @@ abstract class AudioPlayerHelper {
   Future<void> playAudio();
 
   /// This method will be responsible for seeking the audio when dragged forward of reverse
-  /// It will take [Position] of the audio
-  /// This method will seek the audio to specific [Position] given in parameters
+  /// Input: [Duration] to seek the audio to specific [Position]
   Future<void> seekAudio({required Duration position});
 }
 
