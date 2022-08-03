79872c4b05378f9b182fb58919516fe9fddd3e46
maria_jabeen <maria.jabeen@rnssol.com>
2022-08-03 16:40:55 +0500

    update pdf viewer on Published screen

--- lib/screens/clippers/custom_triangle_clipper.dart
+++ lib/screens/clippers/custom_triangle_clipper.dart
@@ -4,9 +4,9 @@ class CustomTriangleClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     final path = Path();
-    path.moveTo(size.width, 0.0);
-    path.lineTo(0.0, size.height);
+    path.moveTo(0, 0.0);
     path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
     path.close();
     return path;
   }
